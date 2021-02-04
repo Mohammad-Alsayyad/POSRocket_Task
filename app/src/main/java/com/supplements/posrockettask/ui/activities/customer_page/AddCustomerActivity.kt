faf11075efdcf9745d29ba896728b4fa915cc7b1
Mohammed-Alsayyad <mohammadhusam198@gmail.com>
2021-02-04 18:58:10 +0200

    Build add customer UI

--- app/src/main/java/com/supplements/posrockettask/ui/activities/customer_page/AddCustomerActivity.kt
+++ app/src/main/java/com/supplements/posrockettask/ui/activities/customer_page/AddCustomerActivity.kt
@@ -9,4 +9,23 @@ class AddCustomerActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_customer)
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val menuInflater = menuInflater
+        menuInflater.inflate(R.menu.menu_add_customer, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_clear -> {
+                finish()
+                true
+            }
+            else -> {
+                super.onOptionsItemSelected(item)
+
+            }
+        }
+    }
 }
